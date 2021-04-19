ff4489d4e44d11ded7bdc77577a4af0da1cb7005
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-04-19 14:46:00 -0500

    terminado proyecto practico react (configuracion del proyecto)

---
+++ src/components/Holamundo.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../assets/styles/App.scss"
+const Holamundo = () =>{
+return(
+    <>
+    <div>Hola mundo!</div>
+    <h1>y desde aqui arriba no se ve na, me escucha hasta tu mama</h1>
+    </>
+)
+}
+
+export default Holamundo
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Holamundo from "./components/Holamundo";
+
+ReactDOM.render(<Holamundo />, document.getElementById("app"));
